66d645f4227942893665a6db13830c0f2489e063
x10x001x <82150516+x10x001x@users.noreply.github.com>
2021-12-23 00:18:27 +0200

    Create Player Towers.lua

---
+++ Player Towers.lua
@@ -0,0 +1,10 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+for _, s in pairs(game.Players:GetPlayers()) do
+print("------------Tower Battles Player Towers------------")
+print(''..s.DisplayName.."'s Tower 1: "..s.StuffToSave.Tower1.Value)
+print(''..s.DisplayName.."'s Tower 2: "..s.StuffToSave.Tower2.Value)
+print(''..s.DisplayName.."'s Tower 3: "..s.StuffToSave.Tower3.Value)
+print(''..s.DisplayName.."'s Tower 4: "..s.StuffToSave.Tower4.Value)
+print(''..s.DisplayName.."'s Tower 5: "..s.StuffToSave.Tower5.Value)
+print("---------------------------------------------------")
+end
