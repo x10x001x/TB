c2f0a1eee213051cace93a37829a83a3552dbfd2
x10x001x <82150516+x10x001x@users.noreply.github.com>
2022-02-03 20:56:40 +0200

    Create MapSkipper.lua

---
+++ MapSkipper.lua
@@ -0,0 +1,25 @@
+    repeat wait(0.5) until game:IsLoaded() and game.Players.LocalPlayer
+    getgenv().map = "Military Base"
+    if game.PlaceId == 49707852 or game.PlaceId == 46955756 then
+    repeat wait()
+    if not workspace.Map1.Type.Value ~= getgenv().map and workspace.Map2.Type.Value ~= getgenv().map and workspace.Map3.Type.Value ~= getgenv().map then
+    workspace.Vote:InvokeServer('Veto')
+    end
+    
+    if workspace.Map1.Type.Value == getgenv().map then
+    workspace.Vote:InvokeServer('Map1')
+    wait(0.1)
+    workspace.SkipWaitVote:InvokeServer()
+    
+    elseif workspace.Map2.Type.Value == getgenv().map then
+    workspace.Vote:InvokeServer('Map2')
+    wait(0.1)
+    workspace.SkipWaitVote:InvokeServer()
+    
+    elseif workspace.Map3.Type.Value == getgenv().map then
+    workspace.Vote:InvokeServer('Map3')
+    wait(0.1)
+    workspace.SkipWaitVote:InvokeServer()
+    end
+    until workspace.Map1.Type.Value == getgenv().map or workspace.Map2.Type.Value == getgenv().map or workspace.Map3.Type.Value == getgenv().map
+    end
